c9e7b4293877a6a2897f19ff5d046ebf596e8882
Steve Park <ghkfkd0323@gmail.com>
2020-07-18 03:21:09 -0700

    add create restaurant server form and delete

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const { render } = require('pug');
 const { Schedule } = db.models;
 
 function asyncHandler(callback) {
@@ -13,24 +14,64 @@ function asyncHandler(callback) {
     }
 }
 
+
+// Index Page for Schedule
 router.get('/', asyncHandler( async( req, res ) => {
     res.render('index')
 }));
 
+
+// Login page for Admin
 router.get('/login', asyncHandler( async( req, res ) => {
     res.render('password')
 }))
 
-router.post('/admin', asyncHandler( async(req, res ) => {
-    console.log('hi')
-    console.log(req.body.admin)
+
+// Enter Admin Page
+router.get('/admin', asyncHandler( async( req, res ) => {
+    const servers = await Schedule.findAll();
+    console.log(servers)
+    res.render('update-schedule', { servers })
+}))
+
+
+// Authentication for Admin Page
+router.post('/admin', asyncHandler( async( req, res ) => {
     const admin = await Schedule.findOne({
         where: { password: req.body.admin }
     })
     if(admin) {
-        res.render('update-schedule')
+        res.redirect('/schedules/admin')
     } else {
         res.redirect('/')
+    }
+}))
+
+// Create Restaurant Server
+router.post('/new', asyncHandler( async( req, res ) => {
+    let server
+    try{server = await Schedule.create({
+        name: req.body.name
+    })
+    res.redirect('/schedules/admin')
+    } catch(error) {
+        if(error.name === "SequelizeValidationError") {
+            server = await Schedule.build(req.body.name);
+            res.render('update-schedule');
+        } else {
+            throw error;
+        }
+    }
+}))
+
+// Delete Restaurant Server
+router.post('/:id/delete', asyncHandler( async( req, res ) => {
+    const server = await Schedule.findByPk(req.params.id);
+    if(server) {
+        await server.destroy();
+        res.rediect('/schedules/admin')
+    } else {
+        res.sendStatus(404)
     }
 }))
 
